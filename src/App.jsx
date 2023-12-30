2b787fbc5236053c2b3d8d208da0c8605841d7d1
MdAthikhasan <mdathikhasan136@gmail.com>
2023-12-30 16:28:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+ import React from 'react'
+import Mainglaout from './Component/Mainlayout/Mainglaout'
+ 
+ const App = () => {
+   return (
+    <Mainglaout/>
+   )
+ }
+ 
+ export default App
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,50 @@
+// import React from "react";
+// import { AiFillAmazonSquare } from "react-icons/ai";
+// import { Link, NavLink } from "react-router-dom";
+// import './header.css'
+// const Header = () => {
+//   return (
+//     <nav>
+//       <div className="nabvar flex">
+//         <div>
+//           <AiFillAmazonSquare />
+//         </div>
+//         <div>
+//           <ul className="flex  ul ">
+//             <abbr title="Home">
+//               <li>
+//                 <NavLink to={"/home"}>Home</NavLink>
+//               </li>
+//             </abbr>
+//             <abbr title="About">
+//               <li>
+//                 <NavLink to={"/about"}>About</NavLink>
+//               </li>
+//             </abbr>
+//             <abbr title="Contact">
+//               <li>
+//                 <NavLink to={"/contact"}>Contact</NavLink>
+//               </li>
+//             </abbr>
+//           </ul>
+//         </div>
+//         <div>
+//           <ul className="flex">
+//             <abbr title="Login">
+//               <li>
+//                 <Link to={"/login"}>Login</Link>
+//               </li>
+//             </abbr>
+//             <abbr title="Log out">
+//               <li>
+//                 <Link >Log out</Link>
+//               </li>
+//             </abbr>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// };
+
+// export default Header;
